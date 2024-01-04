2c29b4fe357b55f03467650793b95404ed77aa5d
hhaa1382 <mehranfarhamid82@gmail.com>
2024-01-04 18:24:49 +0330

    random action add for test

--- project/multiAgents.py
+++ project/multiAgents.py
@@ -1,17 +1,3 @@
-# multiAgents.py
-# --------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
 from util import manhattanDistance
 from game import Directions
 import random, util
@@ -40,25 +26,5 @@ class MultiAgentSearchAgent(Agent):
 
 class AIAgent(MultiAgentSearchAgent):
     def getAction(self, gameState: GameState):
-        """
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
-
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
-
-        # TODO: Your code goes here
-        # util.raiseNotDefined()
+        actions = gameState.getLegalActions(0)
+        return random.choice(actions)
